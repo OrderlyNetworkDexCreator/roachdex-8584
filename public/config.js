c0424cc09966202363751f37148bf58b5c0cd176
Mario Reder <mario.reder@pm.me>
2025-11-02 19:54:09 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Markets,Swap,Portfolio,Vaults,Leaderboard,Rewards",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "Discord, https://discord.gg/jy3FnXBW6e",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/RoachDex",
   "VITE_DISCORD_URL": "https://discord.gg/jy3FnXBW6e",
